f75d62fa652e4305761699828b64769d94d72037
tushar-jaiswal-professional <tusharjaiswal.professional@gmail.com>
2022-07-15 13:32:01 +0530

    Completed

--- app.js
+++ app.js
@@ -26,39 +26,47 @@ function formValidate(){
         errpassword.innerText = "Kindly fill this field";
         errcpassword.innerText = "Kindly fill this field";
         errphone.innerText = "Kindly fill this field";
+        validate.innerText = ""
     }
 
     //Name errors
     if(name.value.length<8 || name.value.length>15){
         errname.innerText = "Atleast 8 to 15 Character Required";
+        validate.innerText = ""
         name.focus();
         return false;
     }
     else{
         errname.innerText = "";
+        validate.innerText = ""
     }
 
     //Address errors
     if(address.value == ""){
         erraddress.innerText = "Kindly fill Address Field";
+        validate.innerText = ""
         address.focus();
         return false;
     }
     else{
         erraddress.innerText = "";
+        validate.innerText = ""
     }
 
     //Email errors
     if(email.value == ""){
         erremail.innerText = "Kindly fill Email Field";
+        validate.innerText = ""
         email.focus();
         return false;
     }
     else if(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email.value)){
         erremail.innerText = "";
+        validate.innerText = ""
     }
     else{
         erremail.innerText = "Email is Invalid";
+        validate.innerText = ""
         email.focus();
         return false;
     }
@@ -66,14 +74,17 @@ function formValidate(){
     //Password errors
     if(password.value == ""){
         errpassword.innerText = "Kindly fill Password Field";
+        validate.innerText = ""
         password.focus();
         return false;
     }
     else if(password.value.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)){
         errpassword.innerText="";
+        validate.innerText = ""
     }
     else{
         errpassword.innerText = "Password must contain 1 uppercase letter, 1 lowercase letter, atleast 1 number and any symbol";
+        validate.innerText = ""
         password.focus();
         return false;
     }
@@ -81,13 +92,16 @@ function formValidate(){
     //Confirm Password errors
     if(cpassword.value == ""){
         errcpassword.innerText = "Kindly fill Confirm Password Field";
+        validate.innerText = ""
         cpassword.focus();
     }
     else if(cpassword.value==password.value){
         errcpassword.innerText = "";
+        validate.innerText = ""
     }
     else{
         errcpassword.innerText = "Password and Confirm Password must be same";
+        validate.innerText = ""
         cpassword.focus();
         return false;
     }
@@ -95,16 +109,19 @@ function formValidate(){
     //Phone errors
     if(phone.value == ""){
         errphone.innerText = "Kindly fill this field";
+        validate.innerText = ""
         phone.focus();
         return false;
     } 
     else if(phone.value.length < 10){
         errphone.innerText = "phone number should contain 10 numbers";
+        validate.innerText = ""
         phone.focus();
         return false;
     }
     else{
         errphone.innerText = "";
+        validate.innerText = ""
     }
 
     validate.innerText = "Regex Validation Success"
